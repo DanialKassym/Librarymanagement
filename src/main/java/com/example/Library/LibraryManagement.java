e1c9a6746f95c072d451de97ae97a5e5ef078329
Danial Kassym <danialkassym@gmail.com>
2026-05-20 00:03:23 +0500

    Initial Commit

---
+++ src/main/java/com/example/Library/LibraryManagement.java
@@ -0,0 +1,12 @@
+package com.example.Library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class LibraryManagement {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagement.class, args);
+	}
+}
---
+++ src/test/java/com/example/Library/LibraryManagementTests.java
@@ -0,0 +1,6 @@
+package com.example.Library;
+
+
+public class LibraryManagementTests {
+
+}

b9dd1a2a326cdf79d4a680c06a61a01baf35056a
Danial Kassym <danialkassym@gmail.com>
2026-05-20 00:51:22 +0500

    Added Author and Category entities. Updated Book entity with ManyToOne relations to tables: author and category

--- src/main/java/com/example/Library/domain/Category.java
+++ src/main/java/com/example/Library/domain/Category.java
@@ -4,25 +4,15 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "books")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-public class Book {
+public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String title;
-
-    private Integer quantity;
-
-    @ManyToOne
-    private Author author;
-
-    @ManyToOne
-    private Category category;
+    private String name;
 }

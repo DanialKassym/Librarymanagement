68be78bf45a48f3d17f7f55b281952114ca95d4f
Danial Kassym <danialkassym@gmail.com>
2026-05-21 18:14:07 +0500

    GloabalExceptionHanlder implemented

--- src/main/java/com/example/Library/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/Library/exception/GlobalExceptionHandler.java
@@ -1,4 +1,29 @@
 package com.example.Library.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(
+            MethodArgumentNotValidException ex
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getBindingResult()
+                        .getFieldError()
+                        .getDefaultMessage());
+    }
 }

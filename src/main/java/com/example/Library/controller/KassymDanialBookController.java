f8c34d042dcd513130cdfc675fced16a9ae0e6f7
Danial Kassym <danialkassym@gmail.com>
2026-05-21 17:44:54 +0500

    BookController endpoints added

--- src/main/java/com/example/Library/controller/KassymDanialBookController.java
+++ src/main/java/com/example/Library/controller/KassymDanialBookController.java
@@ -1,4 +1,44 @@
 package com.example.Library.controller;
 
+import com.example.Library.domain.Book;
+import com.example.Library.service.KassymDanialBookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+@RequiredArgsConstructor
 public class KassymDanialBookController {
+
+    private final KassymDanialBookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Long id) {
+        return bookService.getBookById(id);
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookService.createBook(book);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(
+            @PathVariable Long id,
+            @RequestBody Book book
+    ) {
+        return bookService.updateBook(id, book);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+    }
 }
